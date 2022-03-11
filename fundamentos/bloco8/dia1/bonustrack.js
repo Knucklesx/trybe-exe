b0f222ecd101f44b8541d3c5dcd9143949cfd746
Knuclesx <gcfacchi@gmail.com>
2022-03-11 13:37:53 -0300

    tentativa mage skill

--- fundamentos/bloco8/dia1/bonustrack.js
+++ fundamentos/bloco8/dia1/bonustrack.js
@@ -1,7 +1,7 @@
 const mage = {
     healthPoints: 130,
     intelligence: 45,
-    mana: 125,
+    mana: 0,
     damage: undefined,
 };
   
@@ -33,13 +33,34 @@ const warriorDmg = () => {
     return warriorHit
 }
 
-const mageTurn = () => {
-   let mageDmg = {
-    damageTotal: Math.floor((Math.random() * mage.intelligence)+ (mage.intelligence * 2))
-   }
-
-   return mageDmg
-
+const mageSkillCast = () => {
+    const mageSkillBook = ['Bola de fogo', 'Seta de gelo', 'RAIO CÔSMICOOO']
+    let skillChoose = Math.floor(Math.random()*3) 
+    if (skillChoose === 0) return mageSkillBook[0];
+    if (skillChoose === 1) return mageSkillBook[2]
+    if (skillChoose !== 1 && skillChoose !== 2) return mageSkillBook[3]
 }
+
+
+
+
+const mageTurn = (callback) => {
+   let turnAction = {
+       gastoDeMana: 0,
+       actionTurn: `Sem mana suficiente`
+   };
+   
+   if (mage.mana > 15) {
+    let damageTotal = Math.floor((Math.random() * mage.intelligence)+ (mage.intelligence * 2));
+    turnAction.gastoDeMana = 15;
+    turnAction.actionTurn = damageTotal;
+    return turnAction
+
+
+    }
+    return turnAction
+}
+
+
 
 console.log(mageTurn())
