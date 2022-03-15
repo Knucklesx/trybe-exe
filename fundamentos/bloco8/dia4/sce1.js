278ef2dc0a9bd0aa1c67dbb6e57c21ab4649dd1a
Knuclesx <gcfacchi@gmail.com>
2022-03-15 13:58:28 -0300

    exercicio 2 - resposta gabarito da erro

--- fundamentos/bloco8/dia4/sce1.js
+++ fundamentos/bloco8/dia4/sce1.js
@@ -4,9 +4,17 @@ const arrays = [
     [4, 5, 6],
   ];
   
-  function flatten(array) {
-    return array.reduce((acc, item) => acc.concat(item), [])
+function flatten(array) {
+    return array.reduce((acc, item) => acc.concat(item), [])}
+
+function flatten2(array) {
+    return array.flat()
   }
+
+  // ambas as formas retornam o mesmo resultado
+
 
 
   console.log(flatten(arrays));
+
+  console.log(flatten2(arrays));
