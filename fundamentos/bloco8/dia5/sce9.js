233504f2194c6de2d202cfa270e872573a7b397d
Knuclesx <gcfacchi@gmail.com>
2022-03-16 11:50:16 -0300

    exercicio 9 - obj destucturing e spread operator

---
+++ fundamentos/bloco8/dia5/sce9.js
@@ -0,0 +1,17 @@
+
+const yearSeasons = {
+    spring: ['March', 'April', 'May'],
+    summer: ['June', 'July', 'August'],
+    autumn: ['September', 'October', 'November'],
+    winter: ['December', 'January', 'February'],
+};
+
+
+const seasons = { spring, summer, autumn, winter } = yearSeasons
+
+// console.log(seasons);
+
+
+const months = [...spring, ...summer, ...autumn, ...winter]
+
+console.log(months);
