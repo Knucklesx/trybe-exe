f4692b750bc6f8cecc65213f3bcdd25b25a21e03
Knuclesx <gcfacchi@gmail.com>
2022-03-22 16:58:13 -0300

    fetch passo a passo

--- fundamentos/bloco9/dia2/exercício2/fetch.js
+++ fundamentos/bloco9/dia2/exercício2/fetch.js
@@ -4,6 +4,7 @@ const quemEhEssePokemon = (pokemonName) => {
     const pokemon = fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`)
     const poke = pokemon.then((response) => response.json());
     const mon = poke.then((data) => console.log(data.name))
+    
 }
 
 quemEhEssePokemon()
